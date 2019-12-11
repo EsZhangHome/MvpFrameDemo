761eb655f593e5e5df8893cbd0ca9e6866671348
zes <17600149303@yeah.net>
2019-12-11 10:41:14 +0800

    test develop

--- app/src/main/java/com/zes/mvpframedemo/MainActivity.java
+++ app/src/main/java/com/zes/mvpframedemo/MainActivity.java
@@ -1,14 +1,19 @@
 package com.zes.mvpframedemo;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.tv_test);
+        mTextView.setText("测试");
     }
 }
